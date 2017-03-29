999857c5e8d00ce4ee867a140b5f58179cd32a50
Stefan Santesson <stefan@aaa-sec.com>
2017-03-29 15:11:15 +0200

    License update

--- common/src/main/java/se/tillvaxtverket/tsltrust/common/iaik/PdsQCStatement.java
+++ common/src/main/java/se/tillvaxtverket/tsltrust/common/iaik/PdsQCStatement.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2012 Swedish Agency for Economic and Regional Growth - Tillväxtverket 
+ * Copyright 2017 Swedish E-identification Board (E-legitimationsnämnden)
  *  		 
- * Licensed under the EUPL, Version 1.1 or ñ as soon they will be approved by the 
- * European Commission - subsequent versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the Licence. 
- * You may obtain a copy of the Licence at:
- *
- * http://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software distributed 
- * under the Licence is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and limitations 
- * under the Licence.
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package se.tillvaxtverket.tsltrust.common.iaik;
 
